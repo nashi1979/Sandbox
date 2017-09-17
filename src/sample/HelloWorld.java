4fb4a71a5e8c73aaf74166768627229120e88c9c
hnakane <hnakane@LAPTOP-ICRCNUBJ>
2017-09-17 17:57:10 +0900

    コード修正

--- src/sample/HelloWorld.java
+++ src/sample/HelloWorld.java
@@ -3,5 +3,6 @@ package sample;
 public class HelloWorld {
     public static void main (String[] args) {
         System.out.println("Hello World !!");
+        System.out.println("Sample1 modified!"); // 修正コード
     }
 }
