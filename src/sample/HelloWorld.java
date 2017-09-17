cc48e80eb679290f8ad83b20516310b1b788cbe6
hnakane <hnakane@LAPTOP-ICRCNUBJ>
2017-09-17 18:30:08 +0900

    マージ実験

--- src/sample/HelloWorld.java
+++ src/sample/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main (String[] args) {
         // コメント
         System.out.println("Hello World !!");
-        System.out.println("Sample1 modified!"); // 修正コード
+        System.out.println("Sample1 modified!");
+        System.out.println("Sample2 modified!"); // 修正コード
     }
 }
